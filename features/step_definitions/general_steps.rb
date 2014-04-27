3a78afb7eaef948540c1685e77e61d7c94b69d02
Preeti Gupta <guptapreeti.india@gmail.com>
2014-04-28 01:22:32 +0530

    Added sulekha login feature

--- features/step_definitions/general_steps.rb
+++ features/step_definitions/general_steps.rb
@@ -6,7 +6,7 @@ end
 
 When(/^I log-in in 99acres$/) do
   @user = User.where(id: ENV["USER_ID"]).first
-  @account = @user.accounts.where({accounts: {website_id: 1}}).first
+  @account = @user.accounts.where({accounts: {website_name: MERCHANTS[:acres]}}).first
   step %{I fill in "username" with "#{@account.username}"}
   step %{I fill in "password" with "#{@account.password}"}
   step %{I press "Login >>"}
@@ -14,5 +14,10 @@ end
 
 When(/^I log-in in sulekha$/) do
 	@user = User.where(id: ENV["USER_ID"]).first
-  @account = @user.accounts.where({accounts: {website_id: 1}}).first
+  @account = @user.accounts.where({accounts: {website_name: MERCHANTS[:sulekha]}}).first
+  page.driver.execute_script(%{$("#newSignin").trigger("click")})
+  step %{I fill in "txtEmail" with "#{@account.username}"}
+  step %{I fill in "txtPass" with "#{@account.password}"}
+  page.driver.execute_script(%{$(".control-group.last .HPTrack").trigger("click")})
+  step %{I should break}
 end
